54999a5b4c6998c420cd708d97e4008cb25933f2
Nawal <61576750+lulay2020@users.noreply.github.com>
2020-04-11 15:06:05 +0500

    Update app.js
    use environment variables

--- app.js
+++ app.js
@@ -13,13 +13,14 @@ let express = require('express'),
 	mongoose = require('mongoose');
 	seedDB = require('./seeds');
 
+//requiring routes
 let commentRoutes = require('./routes/comments'),
-	campgroundRoutes = require('./routes/campgrounds'),
- 	indexRoutes = require('./routes/index');
+    campgroundRoutes = require('./routes/campgrounds'),
+    indexRoutes = require('./routes/index');
+
+//configure mongoDB
 // let url = 'mongodb://localhost:27017/yelp-camp', {useNewUrlParser: true, useUnifiedTopology: true}-
-// 'mongodb://mongodb+srv://lulay:realmadrid1998@cluster0-gqb1i.mongodb.net/test?retryWrites=true&w=majority/yelp-camp'
-// console.log(process.env.DATABASEURL)
-mongoose.connect(`mongodb+srv://lulay:realmadrid1998@cluster0-gqb1i.mongodb.net/test?retryWrites=true&w=majority`,
+mongoose.connect(process.env.DATABASEURL,
 {useNewUrlParser: true,
  useCreateIndex: true,
  useUnifiedTopology: true
@@ -33,14 +34,8 @@ app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static(__dirname + '/public'));
 app.use(methodOverride('_method'));
 app.use(flash());
+
 //configure express session for passport package
-// app.use(
-//   require('express-session')({
-//     secret: '5566ThEbEsT',
-//     resave: false,
-//     saveUninitialized: false
-//   })
-// );
 app.use(session({
     secret: "Once again Rusty wins cutest dog!",
     resave: false,
@@ -48,15 +43,19 @@ app.use(session({
     store: new MongoStore({ mongooseConnection: mongoose.connection }),
     cookie: { maxAge: 180 * 60 * 1000 } // 180 minutes session expiration
 }));
+
 //two lines to configure passport
 app.use(passport.initialize());
 app.use(passport.session());
+
 //tell passport-local to use passport-local-mongoose methods
 passport.use(new localStrategy(User.authenticate()))
+
 //passport-local-mongoose methods to encode and decode data in the session
 passport.serializeUser(User.serializeUser())
 passport.deserializeUser(User.deserializeUser())
-//global meddleware 
+
+//global middleware 
 app.use((req, res, next)=>{
 	res.locals.currentUser = req.user;
 	res.locals.error = req.flash('error');
